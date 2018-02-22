46fccc75587fa8ebd7c612f7ccbc6597870bae25
Gram <master_fess@mail.ru>
2018-02-22 17:58:16 +0500

    +config example

---
+++ config_example.py
@@ -0,0 +1,16 @@
+import shelve
+import os
+import random
+import telebot
+import time
+from logger import Log
+
+shelve_name = 'shelve.db'
+token = 'XXX:XXXX'
+chat_id = -0
+"""
+str:str:int
+variable for key-value path/name
+variable for telegram token
+variable for your chat id
+"""
