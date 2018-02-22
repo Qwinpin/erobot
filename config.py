045d9e36cf4bda106832021d5c691a8647221bdc
Gram <master_fess@mail.ru>
2018-02-22 18:19:54 +0500

    new config structure

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
-import shelve
-import os
-import random
-import telebot
-import time
-from logger import Log
-
-shelve_name = 'shelve.db'
-token = 'XXX:XXXX'
-chat_id = -0
-"""
-str:str:int
-variable for key-value path/name
-variable for telegram token
-variable for your chat id
-"""
+SHELVE_NAME = 'shelve.db'
+TOKEN = 'XXX:XXXX'
+CHAT_ID = -0
+LOG_PATH = 'bot.log'
